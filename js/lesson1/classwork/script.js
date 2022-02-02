04136af5edbe72c4a4f73c430189eb3c5a750d59
EvhenAks <aksjonovev@gmail.com>
2022-02-02 16:09:28 +0200

    js cw1 done!

---
+++ js/lesson1/classwork/script.js
@@ -0,0 +1,79 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+//     let numbers = [
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     Math.floor(Math.random() *10),
+//     ];
+// console.log(numbers);
+//
+// let result = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+// let book = {
+//     name: 'song',
+//     numberOfPages: 5,
+//     genre: 'fantasy'
+// };
+// console.log(book);
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+
+// let book = {
+//     name: 'povelitel tayn',
+//     numberOfPages: 1000,
+//     genre: 'fantasy',
+//     author: 'Robert',
+// };
+// console.log(book);
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let books = [
+//     {
+//         name: 'song',
+//         numberOfPages: 5,
+//         genre: 'fantasy',
+//         author: 'Johny'
+//     },
+//     {
+//             name: 'povelitel tayn',
+//             numberOfPages: 1000,
+//             genre: 'fantasy',
+//             author: 'Robert'
+//     }
+// ];
+// console.log(books)
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+//
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+// console.log(s + 'см');
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+// let heightC = 10;
+// let dC = 4;
+// let pi = 3.14;
+// let v = pi * (dC / 2) ** 2 * heightC;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
+// let n = 3;
+// let m = 4;
+// let k = n ** 2 + Math.pow(m, 2);
+// console.log(k);
