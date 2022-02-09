f851935372406ce2fef98ba45ce63eb9b5151dd3
EvhenAks <aksjonovev@gmail.com>
2022-02-09 19:08:59 +0200

    add lesson 4 homework

---
+++ js/lesson4/classwork/main.js
@@ -0,0 +1,54 @@
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+
+// function numberMin(a, b, c) {
+//     if (a < b && a < c) {
+//         console.log(a);
+//     } else if (b < a && b < c) {
+//         console.log(b)
+//     } else console.log(c)
+// }
+//
+// numberMin(3, 5, 7)
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+
+// function numberMax(a, b, c) {
+//     if (a > b && a > c) {
+//         console.log(a)
+//     } else if (b > a && b > c) {
+//         console.log(b)
+//     } else console.log(c)
+// }
+//
+// numberMax(4, 2, 5)
+
+// - створити функцію яка повертає найбільше число з масиву
+
+// let arrNumber = [1, 4, 7, 4, 400, 300, 863, 2, 3, 6, 8, 5]
+//
+// function maxNumber(arr) {
+//     let max = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+// maxNumber(arrNumber)
+
+// - створити функцію яка повертає найменьше число з масиву
+
+// let arrNumber = [7, 4, 400, 0, 300, 863, 2, 3, 6, 8, 5]
+//
+// function minNumber(arr) {
+//     let min = arr[0];
+//     for (let minElement of arr) {
+//         if (minElement < min){
+//             min = minElement
+//         }
+//     }
+//     console.log(min)
+// }
+//
+// minNumber(arrNumber)
