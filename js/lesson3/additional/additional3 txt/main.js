394858fb357cc29bc8d15a0b436387d00796424b
EvhenAks <aksjonovev@gmail.com>
2022-02-12 14:24:03 +0200

    add lesson 5 hw && cw

---
+++ js/lesson3/additional/additional3 txt/main.js
@@ -0,0 +1,38 @@
+// 1.a Створити пустий масив та заповнити його 50 парними числами за допомоги циклу.
+// let arr = [];
+//
+// for (let i = 2; i < 102; i += 2) {
+//     arr.push(i)
+// }
+//
+// console.log(arr)
+
+// 1.b Створити пустий масив та заповнити його 50 непарними числами за допомоги циклу.
+
+// let arr = [];
+//
+// for (let i = 1; i < 100; i += 2){
+//     arr.push(i)
+// }
+//
+// console.log(arr)
+
+// 1.c Створити пустий масив та Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+//
+// let arr = [];
+//
+// for (let i = 0; i < 20; i++) {
+//     arr.push(Math.floor(Math.random()*100))
+// }
+//
+// console.log(arr)
+
+// 1.d Створити пустий масив та Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+
+// let arr = [];
+//
+// for (let i = 0; i < 20; i++) {
+//     arr.push(Math.floor(Math.random() * (732 - 8) + 8))
+// }
+//
+// console.log(arr)
